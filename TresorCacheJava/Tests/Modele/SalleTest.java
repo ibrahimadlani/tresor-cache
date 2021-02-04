1f4e828dc5e0c5457ef4673a65448b6a7220186f
fAlcide <alcide.faucheron@etu.uphf.fr>
2021-02-04 23:05:08 +0100

    Test unitaires valides

--- TresorCacheJava/Tests/Modele/SalleTest.java
+++ TresorCacheJava/Tests/Modele/SalleTest.java
@@ -12,9 +12,29 @@ class SalleTest {
     void isTresor() {
         Salle s = new Salle(0, 1, "SalleTest", new ArrayList<Case>());
         assertFalse(s.isTresor());
+        s.setTresor(true);
+        assertTrue(s.isTresor());
     }
 
     @Test
     void addFantome() {
+        Salle s = new Salle(0, 1, "SalleTest", new ArrayList<Case>());
+        s.addFantome();
+        assertTrue(s.getListeFantomes().size() == 1);
+        s.addFantome();
+        assertTrue(s.getListeFantomes().size() == 2);
+        s.addFantome();
+        assertTrue(s.getListeFantomes().size() == 1);
+        s.addFantome();
+        assertTrue(s.getListeFantomes().size() == 1);
+    }
+
+    @Test
+    void deleteFantome(){
+        Salle s = new Salle(0, 1, "SalleTest", new ArrayList<Case>());
+        s.addFantome();
+        assertTrue(s.getListeFantomes().size() == 1);
+        s.deleteFantome();
+        assertTrue(s.getListeFantomes().size() == 0);
     }
 }
--- TresorCacheJava/src/Modele/Paquet.java
+++ TresorCacheJava/src/Modele/Paquet.java
@@ -9,17 +9,17 @@ public class Paquet {
     public Paquet(Plateau p){
         ArrayList<Carte> paquet = new ArrayList<Carte>();
         paquet.add(new Carte("A",p.getListeSalle().get(0)));
-        paquet.add(new Carte("A",p.getListeSalle().get(1)));
-        paquet.add(new Carte("A",p.getListeSalle().get(2)));
-        paquet.add(new Carte("A",p.getListeSalle().get(3)));
-        paquet.add(new Carte("A",p.getListeSalle().get(4)));
-        paquet.add(new Carte("A",p.getListeSalle().get(5)));
-        paquet.add(new Carte("A",p.getListeSalle().get(6)));
-        paquet.add(new Carte("A",p.getListeSalle().get(7)));
-        paquet.add(new Carte("A",p.getListeSalle().get(8)));
-        paquet.add(new Carte("A",p.getListeSalle().get(9)));
-        paquet.add(new Carte("A",p.getListeSalle().get(10)));
-        paquet.add(new Carte("A",p.getListeSalle().get(11)));
+        paquet.add(new Carte("B",p.getListeSalle().get(1)));
+        paquet.add(new Carte("C",p.getListeSalle().get(2)));
+        paquet.add(new Carte("D",p.getListeSalle().get(3)));
+        paquet.add(new Carte("E",p.getListeSalle().get(4)));
+        paquet.add(new Carte("F",p.getListeSalle().get(5)));
+        paquet.add(new Carte("G",p.getListeSalle().get(6)));
+        paquet.add(new Carte("H",p.getListeSalle().get(7)));
+        paquet.add(new Carte("I",p.getListeSalle().get(8)));
+        paquet.add(new Carte("J",p.getListeSalle().get(9)));
+        paquet.add(new Carte("K",p.getListeSalle().get(10)));
+        paquet.add(new Carte("L",p.getListeSalle().get(11)));
         paquet.add(new Carte("Melange",null));
     }
 
